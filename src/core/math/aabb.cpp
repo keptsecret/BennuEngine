c3591449adf5f414a8e72f8564b0d7bcf61b113e
keptsecret <sorchon@gmail.com>
2023-07-25 15:11:53 +0700

    AABB transforms

--- src/core/math/aabb.cpp
+++ src/core/math/aabb.cpp
@@ -11,17 +11,45 @@ AABB::AABB(const glm::vec3& p1, const glm::vec3& p2) {
 		std::max(p1.z, p2.z) };
 }
 
-void AABB::expand(const glm::vec3& p) {
-	pMin = glm::min(pMin, p);
-	pMax = glm::max(pMax, p);
-}
-
 glm::vec3 AABB::center() {
 	return (pMax + pMin) * 0.5f;
 }
 
 float AABB::radius() {
 	return glm::distance(pMin, pMax) * 0.5f;
+}
+
+void AABB::transform(const glm::mat4& m) {
+	glm::vec3 pmin = pMin, pmax = pMax;
+
+	glm::vec3 translation{ m[3] };
+	glm::mat4 basis{ m[0][0], m[0][1], m[0][2], m[0][3],
+		m[1][0], m[1][1], m[1][2], m[1][3],
+		m[2][0], m[2][1], m[2][2], m[2][3],
+		0, 0, 0, 1 };
+
+	glm::vec3 tmin = translation, tmax = translation;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			float e = basis[i][j] * pmin[j];
+			float f = basis[i][j] * pmax[j];
+			if (e < f) {
+				tmin[i] += e;
+				tmax[i] += f;
+			} else {
+				tmin[i] += f;
+				tmax[i] += e;
+			}
+		}
+	}
+
+	pMin = tmin;
+	pMax = tmax;
+}
+
+void AABB::expand(const glm::vec3& p) {
+	pMin = glm::min(pMin, p);
+	pMax = glm::max(pMax, p);
 }
 
 }  // namespace bennu
--- src/core/math/aabb.h
+++ src/core/math/aabb.h
@@ -10,6 +10,7 @@ public:
 	AABB() {}
 	AABB(const glm::vec3& p1, const glm::vec3& p2);
 
+	void transform(const glm::mat4& m);
 	void expand(const glm::vec3& p);
 
 	glm::vec3 min() { return pMin; }
